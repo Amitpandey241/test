d24030731bb6b29ba11cbd8c2c444c4edca6c5b3
Amipandey241 <pamit1687@gmail.com>
2023-06-11 13:38:01 +0530

    initial commit

---
+++ test.py
@@ -0,0 +1,70 @@
+# # a ="String" #string
+# # b = True  #boolean
+# # c = 1  #integer
+# # d =1.4  # float
+# # #complex
+# # e ='a' # character
+#
+z = [ i for i in range(1,1000) if i%8==0 ]
+print(z)
+#
+#
+test_list = [12, 67, 98, 34]
+# test_list.reverse()
+# print(test_list)
+asd =0
+for i in test_list:
+     asd+=i
+print("THis is sum", asd)
+# res = test_list[-1]
+# # print(res)
+# # sum1=0
+# # sum2 = [ i for i in test_list sum+i]
+# # print(sum1)
+# # d =[]
+# # for i in range(:
+# #      d.append(i)
+# #  print(d)
+#
+#
+# # lis = ["string",1, 1.4,[1,2], 'a',('string1',1), {1:"a"}, ('string',1)]
+# # print(lis[-1][0])
+# # print(lis[-3][0])
+# # n = (1,2,4)
+# # print(type(n))
+#
+
+test = ["a", "b", "ab", "b", 2, 2, 45, 45, "ab" ]
+b = set(test)
+c = list(b)
+print("this is ",c)
+str1 =[]
+intr = []
+a = type("a")
+
+b = type("ad")
+print(b==a)
+for i in c:
+    a = type(i)
+    if a ==b:
+        str1.append(i)
+    else:
+        intr.append(i)
+
+
+print(str1)
+print(intr)
+
+
+
+a = "sumeet"
+a = list(a)
+a.reverse()
+dic ={}
+for i in a:
+    if dic.get(i):
+        dic[i]=dic.get(i)+1
+    else:
+        dic[i]=1
+print(dic)
+del a
